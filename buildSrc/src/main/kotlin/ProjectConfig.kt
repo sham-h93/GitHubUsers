8d39d8c708de75ad0a3eaeefa8a57e9e3366b784
hoseincode <hoseinsadonasl@gmail.com>
2024-05-06 22:49:38 +0330

    remove buildSrc from project

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++
@@ -1,9 +0,0 @@
-object ProjectConfig {
-
-    const val appId = "ir.hoseinsa.githubusers"
-    const val compileSdk = 34
-    const val minSdk = 24
-    const val targetSdk = 34
-    const val versionCode = 1
-    const val versionName = "1.0"
-}
