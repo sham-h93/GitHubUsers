ee62d58852f98d70c0c1bdbf5f7e74fc839e8e08
hoseincode <hoseinsadonasl@gmail.com>
2024-07-24 21:53:17 +0330

    remove unused datastate class in presenter

--- presenter/src/main/java/ir/hoseinsa/presenter/datastate/DataState.kt
+++
@@ -1,7 +0,0 @@
-package ir.hoseinsa.presenter.datastate
-
-sealed class DataState<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : DataState<T>(data = data)
-    class Error<T>(message: String, data: T? = null) : DataState<T>(data, message)
-
-}
